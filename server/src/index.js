ec55ab2a3b4922ce55b2b30c225884a9a15df630
HenriD78 <henri.dargent78@gmail.com>
2025-11-20 00:17:31 +0100

    Set up mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db.js')
+
+dotenv.config()
+
 const app = express()
 const port = 8080
 
+//console.log(process.env.MONGO_URI)
+
 app.get('/', (req, res) => {
-    res.send('')
+    res.send('Server is up and running!')
 })
 
 app.listen(port, () => {
+    connectDB()
     console.log(`Server is running on ${port}`)
-})
+}) 
