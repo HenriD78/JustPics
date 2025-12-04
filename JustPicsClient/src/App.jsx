0c55fbf473a81cfce49b7391ebdb6fb33a203ffc
HenriD78 <henri.dargent78@gmail.com>
2025-12-04 18:00:28 +0100

    created the navbar

--- JustPicsClient/src/App.jsx
+++ JustPicsClient/src/App.jsx
@@ -1,13 +1,13 @@
-import {Box, Button} from "@chakra-ui/react"
+import {Box, useColorModeValue} from "@chakra-ui/react"
 import { Routes, Route} from "react-router-dom"
 import HomePage from "./pages/HomePage.jsx"
 import CreatePage from "./pages/CreatePage.jsx"
-import Navbar from "./components/Navbar.jsx"
+import Navbar from "./components/navbar.jsx"
 // import Footer from "./components/footer.jsx"
 
 function App() {
   return (
-    <Box minH={"100vh"}>
+    <Box minH={"100vh"} bg={useColorModeValue("gray.100", "gray.900")} py={4} mb={8} borderRadius={"md"} >
       <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
